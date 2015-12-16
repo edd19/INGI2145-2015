80682562093e4adc83718c78a9a97859e2b6972a
edd19 <eddyndizera@gmail.com>
2015-12-16 14:08:41 +0100

    user's tweets are save

--- hws/hw2/app/manager/tweet-manager.js
+++ hws/hw2/app/manager/tweet-manager.js
@@ -5,32 +5,52 @@ var async = require('async');
 var collectionName = "Tweets";
 
 var upsertTweet = 'INSERT INTO twitter.Tweets (tweetid, author, created_at, body) '
-    + 'VALUES(?, ?, ?, ?);';
++ 'VALUES(?, ?, ?, ?);';
 
 var openCollection = function(callback) {
-    app.db.collection(collectionName, callback);
+  app.db.collection(collectionName, callback);
 }
 
 /* add a tweet*/
 exports.newTweet = function(data, callback)
 {
-    data.created_at = new Date();
-    data.tweetid = uuid.v1();
-    // HINT:
-    // The data object at this point contains the new tweeet
-    // It has these attributes:
-    // - created_at
-    // - tweetid
-    // - username
-    // - name <- this is the fullname of username
-    // - body
+  data.created_at = new Date();
+  data.tweetid = uuid.v1();
+  // HINT:
+  // The data object at this point contains the new tweeet
+  // It has these attributes:
+  // - created_at
+  // - tweetid
+  // - username
+  // - name <- this is the fullname of username
+  // - body
 
-    // Need to initiate the process that will insert the tweet into the database
-    // and process the tweet for the analytics. These can run in parallel, hence
-    // we suggest you use async.parallel.
-    // This function in the end must call callback(err, data)
+  // Need to initiate the process that will insert the tweet into the database
+  // and process the tweet for the analytics. These can run in parallel, hence
+  // we suggest you use async.parallel.
+  // This function in the end must call callback(err, data)
 
-    async.parallel([
+  async.parallel([
+    function(){
+      try {
+        app.db.execute(upsertTweet,
+          [data.tweetid, data.username, data.created_at, data.body],
+          afterExecution('Error: ', 'Tweet ' + data.tweetid +' upserted.'));
+        } catch (err) {
+          console.log("Error:", err);
+        }
+      },
+      function(){
 
+      }
     ], function (err, results) { callback(err, data); });
-}
+  }
+  function afterExecution(errorMessage, successMessage) {
+    return function(err, result) {
+      if (err) {
+        return console.log(errorMessage + err);
+      } else {
+        return console.log(successMessage);
+      }
+    }
+  }
